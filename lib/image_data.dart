673bc72637dedb2145e8a569acbc170c44146373
hankyuwon <jshfu@naver.com>
2022-10-04 12:27:22 +0900

    `image_data`

---
+++ lib/image_data.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ImageData extends StatelessWidget {
+  final double? width;
+  String icon;
+  ImageData(
+      this.icon, {
+        Key? key,
+        this.width,
+        Color? color,
+      }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      icon,
+      width: MediaQuery.of(context).size.height * 0.25,
+      height: MediaQuery.of(context).size.height * 0.25,
+    );
+  }
+}
+
+class IconsPath {
+  static String get shield => 'assets/images/shield.png';
+}
